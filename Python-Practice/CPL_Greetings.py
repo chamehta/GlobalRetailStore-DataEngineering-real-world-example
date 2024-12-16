389c079dca57abb902bf6b24604bf152ba1bb581
chamehta <chandan.developer22@gmail.com>
2024-12-16 15:28:01 -0700

    Delete Python-Practice directory

--- Python-Practice/CPL_Greetings.py
+++
@@ -1,4 +0,0 @@
-print("Hello World")
-print("Chandandeep Singh Mehta")
-print("Chandandeep S. Mehta")
-print("Sat Sri Akaal")
--- Python-Practice/Chap02/02_07_begin.py
+++
@@ -1,6 +0,0 @@
-print("Hello world!")
-
-print("Goodbye world!")
-
-ls=[1,2,3,4,5]
-print(ls)
